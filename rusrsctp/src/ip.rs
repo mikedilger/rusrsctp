f69ac47b12115fb94cdad6835f682a52bd2df16d
Mike Dilger <mike@optcomp.nz>
2018-11-22 11:05:19 +1300

    impl Ip::sockaddr() functions

--- rusrsctp/src/ip.rs
+++ rusrsctp/src/ip.rs
@@ -1,6 +1,7 @@
 
 use std::net::{Ipv4Addr, Ipv6Addr};
-use rusrsctp_sys::{sockaddr_in, sockaddr_in6, PF_INET, PF_INET6};
+use rusrsctp_sys::{sockaddr_in, sockaddr_in6, PF_INET, PF_INET6,
+                   AF_INET, AF_INET6, in_addr, in6_addr};
 
 pub trait Ip {
     type Addr;
@@ -13,8 +14,15 @@ impl Ip for Ipv4 {
     type Addr = Ipv4Addr;
     type Sockaddr = sockaddr_in;
     fn pf() -> i32 { PF_INET as i32 }
-    fn sockaddr(_addr: Self::Addr, _port: u16) -> Self::Sockaddr {
-        unimplemented!()
+    fn sockaddr(addr: Self::Addr, port: u16) -> Self::Sockaddr {
+        sockaddr_in {
+            sin_family: AF_INET as u16,
+            sin_port: port,
+            sin_addr: in_addr {
+                s_addr: addr.into(),
+            },
+            sin_zero: [0,0,0,0,0,0,0,0],
+        }
     }
 }
 pub struct Ipv6;
@@ -22,7 +30,17 @@ impl Ip for Ipv6 {
     type Addr = Ipv6Addr;
     type Sockaddr = sockaddr_in6;
     fn pf() -> i32 { PF_INET6 as i32 }
-    fn sockaddr(_addr: Self::Addr, _port: u16) -> Self::Sockaddr {
-        unimplemented!()
+    fn sockaddr(addr: Self::Addr, port: u16) -> Self::Sockaddr {
+        sockaddr_in6 {
+            sin6_family: AF_INET6 as u16,
+            sin6_port: port,
+            sin6_flowinfo: 0, // RFC2460 requires zero when not supported
+            sin6_addr: in6_addr {
+                __in6_u: rusrsctp_sys::in6_addr__bindgen_ty_1 {
+                    __u6_addr16: addr.segments()
+                },
+            },
+            sin6_scope_id: 0 // only used for local scope, and we don't set it here
+        }
     }
 }
