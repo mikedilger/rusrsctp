415ef0896e8cb93467d1c2c4b95b2d2e8654ea7a
Mike Dilger <mike@optcomp.nz>
2018-11-27 13:35:54 +1300

    FIX: ip byte order of address/port fields in sockaddr structures

--- rusrsctp/src/ip.rs
+++ rusrsctp/src/ip.rs
@@ -3,6 +3,25 @@ use std::net::{Ipv4Addr, Ipv6Addr};
 use rusrsctp_sys::{sockaddr_in, sockaddr_in6, PF_INET, PF_INET6,
                    AF_INET, AF_INET6, in_addr, in6_addr};
 
+fn u16be(v: u16) -> u16 {
+    if cfg!(target_endian = "little") {
+        ((v & 0x00FF) << 8) | ((v & 0xFF00) >> 8)
+    } else {
+        v
+    }
+}
+
+fn u32be(v: u32) -> u32 {
+    if cfg!(target_endian = "little") {
+        ((v & 0x000000FF) << 24)
+            | ((v & 0x0000FF00) << 8)
+            | ((v & 0x00FF0000) >> 8)
+            | ((v & 0xFF000000) >> 24)
+    } else {
+        v
+    }
+}
+
 pub trait Ip {
     type Addr;
     type Sockaddr;
@@ -19,22 +38,15 @@ impl Ip for Ipv4 {
     fn to_sockaddr(addr: Self::Addr, port: u16) -> Self::Sockaddr {
         sockaddr_in {
             sin_family: AF_INET as u16,
-            sin_port: port,
+            sin_port: u16be(port),
             sin_addr: in_addr {
-                s_addr: addr.into(),
+                s_addr: u32be(addr.into()),
             },
             sin_zero: [0,0,0,0,0,0,0,0],
         }
     }
     fn to_sockaddr_wildcard() -> Self::Sockaddr {
-        sockaddr_in {
-            sin_family: AF_INET as u16,
-            sin_port: 0,
-            sin_addr: in_addr {
-                s_addr: 0,
-            },
-            sin_zero: [0,0,0,0,0,0,0,0],
-        }
+        Self::to_sockaddr(Ipv4Addr::UNSPECIFIED, 0)
     }
     fn from_sockaddr(sockaddr: Self::Sockaddr) -> (Self::Addr, u16) {
         (sockaddr.sin_addr.s_addr.into(), sockaddr.sin_port)
@@ -46,30 +58,24 @@ impl Ip for Ipv6 {
     type Sockaddr = sockaddr_in6;
     fn pf() -> i32 { PF_INET6 as i32 }
     fn to_sockaddr(addr: Self::Addr, port: u16) -> Self::Sockaddr {
+        let mut segments = addr.segments();
+        for i in 0..8 {
+            segments[i] = u16be(segments[i]); // convert to big endian
+        }
         sockaddr_in6 {
             sin6_family: AF_INET6 as u16,
-            sin6_port: port,
+            sin6_port: u16be(port),
             sin6_flowinfo: 0, // RFC2460 requires zero when not supported
             sin6_addr: in6_addr {
                 __in6_u: rusrsctp_sys::in6_addr__bindgen_ty_1 {
-                    __u6_addr16: addr.segments()
+                    __u6_addr16: segments
                 },
             },
             sin6_scope_id: 0 // only used for local scope, and we don't set it here
         }
     }
     fn to_sockaddr_wildcard() -> Self::Sockaddr {
-        sockaddr_in6 {
-            sin6_family: AF_INET6 as u16,
-            sin6_port: 0,
-            sin6_flowinfo: 0, // RFC2460 requires zero when not supported
-            sin6_addr: in6_addr {
-                __in6_u: rusrsctp_sys::in6_addr__bindgen_ty_1 {
-                    __u6_addr16: [0, 0, 0, 0, 0, 0, 0, 0]
-                },
-            },
-            sin6_scope_id: 0 // only used for local scope, and we don't set it here
-        }
+        Self::to_sockaddr(Ipv6Addr::UNSPECIFIED, 0)
     }
     fn from_sockaddr(sockaddr: Self::Sockaddr) -> (Self::Addr, u16) {
         unsafe {
